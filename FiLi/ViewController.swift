bda63a4cf5514a7a2ad2e8a923243001025744e4
Kanneka <bunthuonkanneka@gmail.com>
2022-07-01 00:47:14 +1000

    layout part 1 and 2 and some view controllers connected

--- FiLi/ViewController.swift
+++ FiLi/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func answer1(_ sender: Any) {
+    }
+    @IBAction func answer2(_ sender: Any) {
+    }
+    @IBAction func answer3(_ sender: Any) {
+    }
+    
+    var answer = [""]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
